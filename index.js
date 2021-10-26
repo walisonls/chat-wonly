dea03190278e9303e7c5220568ecbdbc201b29c5
sestradiano <gwalisonls@gmail.com>
2021-10-26 09:38:23 -0300

    Enter

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-const { Socket } = require("socket.io");
+const app = require("express")();
 
-const express = require("express")
-const app = express();
 const http = require("http").createServer(app);
-// npm install express socket.io
+
 const io = require("socket.io")(http);
-
-app.use("/public",express.static("public"))
-
 
 var usuarios = [];
 
+var socketIds = [];
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/pages/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
+  socket.on("new user", function (data) {
+    if (usuarios.indexOf(data) != -1) {
+      socket.emit("new user", { success: false });
+    } else {
+      usuarios.push(data);
 
-  socket.on('chat message',(obj)=>{
-    io.emit("chat message", obj);
-  })
+      socketIds.push(socket.id);
 
-  
+      socket.emit("new user", { success: true });
+
+      //Emit para os outros usuários dizendo que tem um novo usuário ativo.
+
+      //socket.broadcast.emit("hello", "world");
+    }
+  });
+
+  socket.on("chat message", (obj) => {
+    if (
+      usuarios.indexOf(obj.nome) != -1 &&
+      usuarios.indexOf(obj.nome) == socketIds.indexOf(socket.id)
+    ) {
+      io.emit("chat message", obj);
+    } else {
+      console.log("Erro: Você não tem permissão para executar a ação.");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    let id = socketIds.indexOf(socket.id);
+
+    socketIds.splice(id, 1);
+
+    usuarios.splice(id, 1);
+
+    console.log(socketIds);
+
+    console.log(usuarios);
+
+    console.log("user disconnected");
+  });
 });
 
 http.listen(3000, () => {
